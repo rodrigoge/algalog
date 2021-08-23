e72f62fdf46614abd4003b7cd596b30c8aaf36e7
rodrigoge <rgestevao@gmail.com>
2021-08-22 21:58:50 -0300

    finishing project api

--- algalog-api/src/main/java/com/algaworks/algalog/domain/model/Entrega.java
+++ algalog-api/src/main/java/com/algaworks/algalog/domain/model/Entrega.java
@@ -2,7 +2,10 @@ package com.algaworks.algalog.domain.model;
 
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
@@ -11,10 +14,10 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotNull;
-import javax.validation.groups.ConvertGroup;
-import javax.validation.groups.Default;
-import com.algaworks.algalog.domain.ValidationGroups;
+
+import com.algaworks.algalog.domain.exception.NegocioException;
 
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -32,7 +35,6 @@ public class Entrega {
 	private Long id;
 	
 	@ManyToOne
-	@ConvertGroup(from = Default.class, to = ValidationGroups.ClienteId.class)
 	private Cliente cliente;
 	
 	@Embedded
@@ -41,11 +43,43 @@ public class Entrega {
 	@NotNull
 	private BigDecimal taxa;
 	
+	@OneToMany(mappedBy = "entrega", cascade = CascadeType.ALL)
+	private List<Ocorrencia> ocorrencias = new ArrayList<>();
+	
 	@Enumerated(EnumType.STRING)
 	private StatusEntrega status;
 	
 	private OffsetDateTime dataPedido;
 	
 	private OffsetDateTime dataFinalizacao;
+
+	public Ocorrencia adicionarOcorrencia(String descricao) {
+		Ocorrencia ocorrencia = new Ocorrencia();
+		ocorrencia.setDescricao(descricao);
+		ocorrencia.setDataRegistro(OffsetDateTime.now());
+		ocorrencia.setEntrega(this);
+		
+		this.getOcorrencias().add(ocorrencia);
+		
+		return ocorrencia;
+	}
+
+	public void finalizar() {
+		if(naoPodeSerFinalizada()) {
+			throw new NegocioException("Entrega não pode ser finalizada");
+		}
+		
+		setStatus(StatusEntrega.FINALIZADA);
+		setDataFinalizacao(OffsetDateTime.now());
+		
+	}
+	
+	public boolean podeSerFinalizada() {
+		return StatusEntrega.PENDENTE.equals(getStatus());
+	}
+	
+	public boolean naoPodeSerFinalizada() {
+		return !podeSerFinalizada();
+	}
 	
 }
